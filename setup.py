e523139948a852d60ceb543eed2ce6e7de619c59
Gp1991 <Diciannovenne_19>
2021-01-08 16:04:19 +0100

    various update

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
-import setuptools
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="guascoigne", # Replace with your own username
-    version="0.0.1",
-    author="gabrieelepinto",
-    author_email="gabriele.pinto@uniroma1.it",
-    description="a set of useful tools for python. at the moment it includes: coeffplot a tool to make coefficient regression plot with python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/gabrielepinto/gascoigne",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+from distutils.core import setup
+setup(
+  name = 'gascoigne',         # How you named your package folder (MyLib)
+  packages = ['gascoigne'],   # Chose the same as "name"
+  version = '0.2',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'package with different tools',   # Give a short description about your library
+  author = 'Gabriele Pinto',                   # Type in your name
+  author_email = 'gabriele.pinto@uniroma1.it',      # Type in your E-Mail
+  url = 'https://github.com/gabrielepinto/gascoigne',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['coefficient plot', 'regression coefficient', 'coeffplot',"regression coefficient plot"],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'matplotlib',
+          'numpy',
+          "pandas",
+          "seaborn"
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
